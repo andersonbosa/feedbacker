56599b6c7a4a6e077e33e35609dfb8811348a292
andersonbosa <andersonbosa0@gmail.com>
2023-03-04 21:18:11 -0300

    Update JWT_SECRET to JWT_API_SECRET and add public resources to router

--- services/feedbacker-api/src/routes/index.mjs
+++ services/feedbacker-api/src/routes/index.mjs
@@ -5,7 +5,9 @@ import { CreateApiKeyHandler } from '../handlers/apikey.mjs'
 import { CreateAuthHandler } from '../handlers/auth.mjs'
 import { CreateFeedbackHandler } from '../handlers/feedbacks.mjs'
 import { CreateUserHandler } from '../handlers/users.mjs'
-const { JWT_SECRET } = process.env
+
+
+const { JWT_API_SECRET } = process.env
 
 
 /* initialize handlers */
@@ -13,7 +15,6 @@ const feedbacksHandler = CreateFeedbackHandler(databaseClient)
 const usersHandler = CreateUserHandler(databaseClient)
 const authHandler = CreateAuthHandler(databaseClient)
 const apiKeyHandler = CreateApiKeyHandler(databaseClient)
-const authMiddleware = jwt({ secret: JWT_SECRET })
 
 
 /* router definitions */
@@ -21,23 +22,20 @@ const router = new Router()
 
 router.get('/healthcheck', async (ctx) => {
   ctx.status = 200
-
-  ctx.body = {
-    alive: true,
-    message: new Date
-  }
+  ctx.body = { alive: true, message: new Date }
 })
 
-router.head('/apikey/exists', apiKeyHandler.checkIfApiKeyExists)
-
+/* Public resources */
 router.post('/auth/register', usersHandler.create)
 router.post('/auth/login', authHandler.login)
+router.post('/feedbacks', feedbacksHandler.create)
+router.head('/apikey/exists', apiKeyHandler.checkIfApiKeyExists)
 
+/* Secured resources */
+const authMiddleware = jwt({ secret: JWT_API_SECRET })
 router.get('/users/me', authMiddleware, usersHandler.getLoggerUser)
 router.post('/users/me/apikey', authMiddleware, usersHandler.generateApiKey)
-
 router.get('/feedbacks', authMiddleware, feedbacksHandler.getFeedbacks)
-router.post('/feedbacks', feedbacksHandler.create)
 router.get('/feedbacks/summary', authMiddleware, feedbacksHandler.getSummary)
 
 
