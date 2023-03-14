e95c541eecf3b9586a54841ddbe116a47653d634
andersonbosa <andersonbosa0@gmail.com>
2023-03-13 22:39:49 -0300

    checkpoint

--- services/feedbacker-app/src/components/Feedbacks/FiltersLoading.vue
+++ services/feedbacker-app/src/components/Feedbacks/FiltersLoading.vue
@@ -2,17 +2,14 @@
   <ContentLoader class="flex flex-col items-center rounded" width="100%" height="300px">
 
     <ContentLoader class="mt-3 rounded" width="90%" height="50px" animation-duration="2s" />
-
     <ContentLoader class="mt-3 rounded" width="80%" height="50px" animation-duration="2.3s" />
-
     <ContentLoader class="mt-3 rounded" width="80%" height="50px" animation-duration="2.7s" />
-
     <ContentLoader class="mt-3 rounded" width="80%" height="50px" animation-duration="2.9s" />
+
   </ContentLoader>
 </template>
 
 <script setup lang="ts">
 import ContentLoader from '~/components/Feedbacks/ContentLoader/index.vue'
-
 
 </script>
