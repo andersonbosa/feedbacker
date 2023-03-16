91a118024679ac1af354654e282f7072e0089b17
andersonbosa <andersonbosa0@gmail.com>
2023-03-16 09:10:10 -0300

    checkpoint

--- services/feedbacker-app/src/components/ModalFactory/index.vue
+++ services/feedbacker-app/src/components/ModalFactory/index.vue
@@ -1,54 +1,77 @@
-<template>
-  <teleport to="body">
-    <div v-if="modal.isActive"
-      class="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50"
-      @click="handleModalToogle({ status: false })">
-      <div class="fixed mx-10" :class="modal.width" @click.stop>
-        <div
-          class="flex flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeInDown animate__faster">
-          <div class="flex flex-col px-12 py-10 bg-white">
-            <component :is="modal.component" />
-          </div>
-        </div>
-
-      </div>
-    </div>
-  </teleport>
-</template>
-
 <script setup lang="ts">
-// import { onBeforeUnmount, onMounted } from 'vue'
-import ModalAccountCreate from '~/components/ModalCreateAccount/index.vue'
-import ModalLogin from '~/components/ModalLogin/index.vue'
+console.log('EU SOU MODAL FACTORY');
 
-const modal = useModal()
-
-onMounted(() => {
-  modal.listen(handleModalToogle)
-})
-
-onBeforeUnmount(() => {
-  modal.off(handleModalToogle)
-})
-
-function handleModalToogle (mutationObject: any, state: any) {
-  console.log(' 🟡 handleModalToogle:',)
-
-  const analyzedValue = mutationObject.events.newValue // através do $subscribe do pinia
-  // const analyzedValue = state.payload // através do useModalStore
-
-  if (analyzedValue.status) {
-    modal.component = analyzedValue.component
-    modal.props = analyzedValue.props
-    modal.width = analyzedValue.width || modal.store.getDefaultModalWidth
-  } else {
-    /* reseta o bagui ??? */
-    modal.component = {}
-    modal.props = {}
-    modal.width = modal.store.getDefaultModalWidth
+const state = {
+  email: {
+    errorMessage: '',
+    value: ''
+  },
+  hasErrors: false,
+  isLoading: false,
+  password: {
+    errorMessage: '',
+    value: ''
   }
+}
 
-  modal.isActive = analyzedValue.status
+function close () {
+  console.log('f:close()')
+}
+
+function handleSubmit () {
+  console.log('f:handleSubmit')
 }
 
 </script>
+
+<template>
+  <div>
+    <div class="flex justify-between" id="modal-login">
+      <h1 class="text-4xl font-black text-gray-800">
+        Entre na sua conta
+      </h1>
+
+      <button @click="close" class="text-4xl text-gray-600 focus:outline-none">
+        &times;
+      </button>
+    </div>
+
+
+    <div class="mt-16">
+      <form @submit.prevent="handleSubmit">
+        <label class="block">
+          <span class="text-lg font-medium text-gray-800">E-mail</span>
+          <input id="email-field" v-model="state.email.value" type="email" :class="{
+            'border-brand-danger': !!state.email.errorMessage
+          }" class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+            placeholder="jane.dae@gmail.com">
+          <span id="email-error" v-if="!!state.email.errorMessage" class="block font-medium text-brand-danger">
+            {{ state.email.errorMessage }}
+          </span>
+        </label>
+
+        <label class="block mt-9">
+          <span class="text-lg font-medium text-gray-800">Senha</span>
+          <input id="password-field" v-model="state.password.value" type="password" :class="{
+            'border-brand-danger': !!state.password.errorMessage
+          }" class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+            placeholder="jane.dae@gmail.com">
+          <span v-if="!!state.password.errorMessage" class="block font-medium text-brand-danger">
+            {{ state.password.errorMessage }}
+          </span>
+        </label>
+
+        <button id="submit-button" :disabled="state.isLoading" type="submit" :class="{
+          'opacity-50': state.isLoading
+        }"
+          class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150">
+          <icon v-if="state.isLoading" name="loading" class="animate-spin" />
+          <span v-else>Entrar</span>
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+
+<style lang="postcss" scoped></style>
